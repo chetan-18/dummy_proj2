fd41a836331c2ebcfc0aeda33f3c017b59521434
chetan <patilchetanskumar@gmail.com>
2021-12-14 18:59:41 +0530

    creating & applying update and edit actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,33 @@ class UsersController < ApplicationController
     def new
         @user = User.new 
     end 
-    # new and create action both act upon new.html.erb
+    # new and create action both acts upon new.html.erb
+    def edit 
+        @user = User.find(params[:id])     # this will find user based on id
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to articles_path
+        else 
+            render 'edit'
+        end 
+    end 
+
     def create 
         # byebug
         @user = User.new(user_params)
         if @user.save
-            flash[:notice] = "Welcome to project,#{@user.username} you have succefully signed up"
+            flash[:notice] = "Welcome to project,#{@user.username} you have successfully signed up"
             redirect_to articles_path
         else  
             render 'new'
         end 
-    end 
+    end
+    
+
 
     private 
     def user_params 
