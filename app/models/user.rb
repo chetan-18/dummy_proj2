caa41b2c22dbb05f128a8dbcd2f038fe9af6892a
chetan <patilchetanskumar@gmail.com>
2021-12-14 09:30:58 +0530

    adding user link in html, applying before_save in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord 
+    before_save {self.email = email.downcase}       # here self refers to current object
     has_many :articles
     validates :username, presence: true,
                          uniqueness: {case_sensitive: false},
